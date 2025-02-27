72d85f664a0f260f7246f08d8c8d67c7ff83425f
DKHISSI Zouhair <zouhair.dkhissi@etu.univ-cotedazur.fr>
2025-02-27 22:13:35 +0100

    maj10

--- app/api/router.py
+++ app/api/router.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
-from app.api.endpoints import document
-from app.api.endpoints import chroma
-from app.api.endpoints import chat
+from app.api.endpoints import document, chroma, chat, conversations
 
 router = APIRouter()
 router.include_router(document.router, prefix="/api", tags=["documents"])
 router.include_router(chroma.router, prefix="/api", tags=["chroma"])
 router.include_router(chat.router, prefix="/api", tags=["chat"])
+router.include_router(conversations.router, prefix="/api", tags=["conversations"])
